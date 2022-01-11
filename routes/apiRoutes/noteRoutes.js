9c69e73f023e0fe4652fe76ed065f5301c75faa5
noah smith <noahjosephsmith@gmail.com>
2022-01-11 16:19:27 -0700

    deployed heroku 121

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const {notes} = require('../../db/db');
-const {newNote}=require('../../lib/noteFunctions');
+const {newNote} = require('../../lib/noteFunctions');
 
 router.get('/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 })
+
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = newNote(req.body, notes);
     res.json(note);
-});
+})
 module.exports = router;
