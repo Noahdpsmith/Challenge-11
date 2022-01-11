3a9a227214318272b8d9af25072a56bd53e46142
noah smith <noahjosephsmith@gmail.com>
2022-01-11 14:57:11 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const apiRoutes = require('./routes/noteRoutes');
 const htmlRoutes = require('./routes/index');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
