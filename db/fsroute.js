01cf229ad97e167bb9e7c1eae722d31b7752b4e9
noah smith <noahjosephsmith@gmail.com>
2022-01-11 15:10:22 -0700

    deployed heroku

--- db/fsroute.js
+++ db/fsroute.js
@@ -4,7 +4,7 @@ function newNote(all, notesArray){
     const note = all;
     notesArray.push(note);
     fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'),
+        path.join(__dirname, '../db/db'),
         JSON.stringify({
             notes: notesArray
         }, null, 2)
